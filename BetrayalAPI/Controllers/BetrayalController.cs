0f2e3f7b7116174273018eec4a52f562ad9fbc61
colmprunty <colm.prunty@teamdatalogic.com>
2014-01-16 23:38:03 +0000

    getting a list

--- BetrayalAPI/Controllers/BetrayalController.cs
+++ BetrayalAPI/Controllers/BetrayalController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http.Cors;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Cors;
 using BetrayalAPI.Models;
 
 namespace BetrayalAPI.Controllers
@@ -6,12 +8,19 @@ namespace BetrayalAPI.Controllers
     [EnableCors(origins: "http://localhost:5454", headers: "*", methods: "*")]
     public class BetrayalController : RavenController
     {
+        public List<Character> GetCharacters()
+        {
+            return Session.Query<Character>().ToList();
+        } 
+
         public void Reset()
         {
             foreach (var character in Session.Query<Character>())
             {
                 Session.Delete(character);
             }
+
+            Session.SaveChanges();
 
             var ox = new Character
                      {
@@ -27,8 +36,23 @@ namespace BetrayalAPI.Controllers
                          Name = "Ox Bellows",
                      };
 
-
             Session.Store(ox);
+
+            var flash = new Character
+            {
+                Id = 2,
+                Knowledge = new[] { 0, 2, 3, 3, 4, 5, 5, 5, 7 },
+                CurrentKnowledge = 3,
+                Sanity = new[] { 0, 1, 2, 3, 4, 5, 5, 5, 7 },
+                CurrentSanity = 3,
+                Might = new[] { 0, 2, 3, 3, 4, 5, 6, 6, 7 },
+                CurrentMight = 3,
+                Speed = new[] { 0, 4, 4, 4, 5, 6, 7, 7, 8 },
+                CurrentSpeed = 5,
+                Name = "Darrin 'Flash' Williams",
+            };
+
+            Session.Store(flash);
         }
     }
 }
--- Frontend/app.js
+++ Frontend/app.js
@@ -4,4 +4,8 @@ angular.module('BetrayalApp', ['ngRoute'])
 	$scope.reset = function(){
 		$http.post('http://localhost:59858/api/betrayal/Reset');
 	};
+
+	$http.get('http://localhost:59858/api/betrayal/GetCharacters').success(function(data){
+		$scope.characters = data;
+	});
 });
